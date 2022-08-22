1cdae66c56af955eb35cef3de0ec9283cf05bcd8
megret franck <msynthese@gmail.com>
2022-08-22 16:14:31 +0200

    starting setup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+
   def home
   end
 end
