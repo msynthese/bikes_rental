53c4df32e226b57dbae4a2c20ec11ca1fd2f7e54
megret franck <msynthese@gmail.com>
2022-08-23 07:19:54 +0200

    Modification des colonnes first_name, last_name

--- db/migrate/20220822185436_add_firstname_to_users.rb
+++ db/migrate/20220822185436_add_firstname_to_users.rb
@@ -1,5 +1,5 @@
 class AddFirstnameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :firstname, :string
+    add_column :users, :first_name, :string
   end
 end
--- db/migrate/20220822185454_add_lastname_to_users.rb
+++ db/migrate/20220822185454_add_lastname_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastnameToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :lastname, :string
+    add_column :users, :last_name, :string
   end
 end
