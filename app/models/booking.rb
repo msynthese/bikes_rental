da5bb16d14da0d092ab45101b6860705be375fe4
Joao Henriques <joao33ti@gmail.com>
2022-08-23 14:40:10 +0200

    controller booking standby

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
+  # validates :title, presence: true
+  # validates :body, presence: true
+  # enum bookingStatuses:
+
+  # validates :bookingnStatus, inclusion: { in: reservationStatuses }
+  # validates :checkOutTime, presence: true
+  # validates :expectedReturnTime, presence: true
   belongs_to :user
   belongs_to :bike
   has_many :bikes, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
+  root to: "booking#index"
+
+  get "/bookings", to: "bookings#index"
+  get "/bookings/:id", to: "bookings#show"
+
+  resources :bookings
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
