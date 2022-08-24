52befb14556741c9512513e70fdf80879e9e0c0e
megret franck <msynthese@gmail.com>
2022-08-24 22:14:21 +0200

    code js search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name owner])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name owner longitude latitude])
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name owner])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name owner longitude latitude])
   end
 end
