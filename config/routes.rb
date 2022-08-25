37d14bb3722070d13840286827a88593653a96de
Joao Henriques <joao33ti@gmail.com>
2022-08-25 18:14:24 +0200

    DialogConfirmStatus

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :bikes, only: %i[new create index show]
-  resources :bookings, only: %i[new show]
+  resources :bookings, only: %i[index show]
+  resources :bikes, only: %i[new create index show] do
+    resources :bookings, only: %i[new create]
+  end
   get "my_bikes", to: "bikes#bikes_owner"
+  # get '/bookings/:id/', to: "bookings#new"
+  # get "bookings", to: "bookings#bookings/new/create"
 end
